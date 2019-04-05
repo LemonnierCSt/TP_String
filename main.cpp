ab4ac53c9fcb9a086a83248db06ce5aee547684d
Armande Cirot <armande.cirot@insa-lyon.fr>
2019-04-05 18:39:15 +0200

    Added Tests length max_size
    Added Tests to length and max_size
    
    Signed-off-by: Armande Cirot <armande.cirot@insa-lyon.fr>

--- main.cpp
+++ main.cpp
@@ -6,18 +6,25 @@ int main(){
   size_t cap=10;
   String str(cap);
   
+
+  
   //Armande's tests
   char* loup = new char [5];
   loup[0]='l'; loup[1]='o'; loup[2]='u'; loup[3]='p'; loup[4]='\0';
   char* nothing = new char[5];
   nothing[0]='\0'; nothing[1]='a'; nothing[2]='b';
-  
+  String nulstr(nothing);
   
   //Using constructor c-string
   String Stringloup(loup);
   
-  std::cout << Stringloup.length() << std::endl;
-  std::cout << Stringloup.max_size() << std::endl;
+  //Test method length()
+  std::cout << (nulstr.length()==0) << std::endl;
+  std::cout << (Stringloup.length()==4) << std::endl;
+  
+  //Test method max_size()
+  std::cout << (Stringloup.max_size()==100) << std::endl;
+  std::cout << (nulstr.max_size()==100) << std::endl;
    
   return 0;
 }
