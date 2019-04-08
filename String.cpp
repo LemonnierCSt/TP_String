598576e851017d792969185ea4eb892b02403551
Armande Cirot <armande.cirot@insa-lyon.fr>
2019-04-08 14:22:01 +0200

    Added comments
    Explaining functions length, max_size, resize, and constructor c-string
    
    Signed-off-by: Armande Cirot <armande.cirot@insa-lyon.fr>

--- String.cpp
+++ String.cpp
@@ -9,6 +9,7 @@ String::String(size_t capacity){
   data_[0]='t';data_[1]='e';data_[2]='s';data_[3]='t'; data_[4]='\0';
 }
 
+//Constructor c-string (by parameter)
 String::String(const char* chain){
   
   size_t nbchar = 0;
@@ -20,27 +21,30 @@ String::String(const char* chain){
     if(nbchar<50){
       capacity_ = size_*2;  //If I can, I allocate twice the size_ of my chain
     } else if((nbchar>50) & (nbchar<100)){
-      capacity_ = size_+1;
+      capacity_ = size_+1;  //If I can't, I allocate one space more than the size of my chain
     }
     data_ = new char[nbchar+1]; //I add a space for the '\0'
     for (size_t i =0; i < nbchar; i++){  //I create a new chain like the one in parameters.
       data_[i]=chain[i];
     }
-    data_[nbchar+1] = '\0';
+    data_[nbchar+1] = '\0'; //I add '\0' to end the string
     }
     else {
-    std::cout << "Your chain is too long : 100 char max please" << std::endl;
+    std::cout << "Your chain is too long : 100 char max please" << std::endl; //If the chain is too long, eroor message
   }
 }
 
+//Method length: returns the size of the string
 size_t String::length(){
   return size_;
 }
 
+//Method max_size: returns the maximum size a string can reach
 size_t String::max_size(){
   return MAX_SIZE;
 }
 
+//Method resize: takes in parameter the position of the last character, and shortens the string
 void String::resize(size_t n){
   if(n <= size_){
     data_[n]= '\0';
@@ -84,7 +88,6 @@ const char* String::c_str() const{
 //Getter of the current length of the string, in terms of bytes.
 size_t String::size() const{
   return size_;   
-    
 }
-  
+
 
